65843c0356fa2711f26bd0cd7b886b5b8a816980
Mark McConnell <amark.mcconnell@gmail.com>
2022-05-17 14:54:45 -0700

    update project.
    Co-authored-by: evmeshuris <evmeshuris@users.noreply.github.com>

---
+++ AnimalShelter/Controllers/AnimalController.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using AnimalShelter.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Net;
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Controllers
+{
+  public class AnimalsController : Controller
+  {
+    private readonly AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Animal> model = _db.Animals.ToList();
+      List<Animal> animalsByType = model.OrderBy(animals => animals.Type).ToList();
+      return View(animalsByType);
+    }
+    
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    public ActionResult Details(int id)
+{
+    Animal thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+    return View(thisAnimal);
+}
+
+    [HttpPost]
+    public ActionResult Create(Animal animal)
+    {
+        _db.Animals.Add(animal);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    public ActionResult Delete(int id)
+    {
+        var thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+        return View(thisAnimal);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+        _db.Animals.Remove(thisAnimal);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    public ActionResult Edit(int id)
+    {
+        var thisAnimal = _db.Animals.FirstOrDefault(animal => animal.AnimalId == id);
+        return View(thisAnimal);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Animal animal)
+    {
+        _db.Entry(animal).State = EntityState.Modified;
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+  }
+}
---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+
+
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string DateOfA { get; set;}
+    public string Breed { get; set; }
+
+  }
+}
